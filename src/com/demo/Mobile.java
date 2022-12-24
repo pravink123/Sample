f0ba23cf2d0247b74efe54d5d8eaa36f1ba1bf38
pravink123 <pravinkasabe.pk145@gmail.com>
2022-12-24 21:06:51 +0530

    first Comment : first array

---
+++ src/com/demo/Mobile.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		 List<String> list=new ArrayList<String>();
+		 list.add("Pravin");
+		 list.add("Suraj");
+		 list.add("Datta");
+		 
+		 System.out.println(list);
+	}
+
+	}
+
+
